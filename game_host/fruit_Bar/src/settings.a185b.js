774a9fe1f9f5df51503b3e1750d33119393858b9
Bolicrad <815763627@qq.com>
2022-02-22 00:05:04 -0700

    Site updated: 2022-02-22 00:05:03

---
+++ game_host/fruit_Bar/src/settings.a185b.js
@@ -0,0 +1,31 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "fbo"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/prefabs/Icons.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/scripts/ssr/svg/util/tess2.b0940.js"
+    ],
+    bundleVers: {
+        resources: "6d8d6",
+        internal: "cb4f5",
+        main: "4e405"
+    }
+};
